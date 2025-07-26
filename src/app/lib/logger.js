84e3b76488952a58bc0df5ed47721c3c90f8c5ac
ShMirnajafi <shahriar.mirnajafizade@gmail.com>
2025-07-26 05:02:32 +0330

    add resource logging

--- src/app/lib/logger.js
+++ src/app/lib/logger.js
@@ -2,3 +2,8 @@
 export function logEvent(event, details = {}) {
     console.log(`[${new Date().toISOString()}] ${event}`, details);
 }
+// simple performance logger
+setInterval(() => {
+    console.log("🧠 Heap:", process.memoryUsage().heapUsed);
+    console.log("⚙️ CPU load:", process.cpuUsage());
+}, 60000);
